74cf9d1fd749279d433e91f83c075f1684c3a7c5
Raphael Eiselstein <github@freibyter.de>
2013-11-17 01:00:20 +0100

    all in one

--- files_checksums.sh
+++ files_checksums.sh
@@ -3,30 +3,25 @@
 sqlite3 files_checksums.sqlite3 <<EOF
 .mode column
 .headers on
+
 .print
 .print --> All Files
 select * from files;
+
 .print
 .print --> All Checksums
 select * from checksums;
-EOF
 
-sqlite3 files_checksums.sqlite3 <<EOF
 .print
 .print --> Get content attribute foo for every file 
-.mode column
-.headers on
 .width 4 20 20
 select f.id,f.file_path,x.foo
 from files as f 
 	inner join checksums as x on f.checksum=x.checksum;
-EOF
 
-sqlite3 files_checksums.sqlite3 <<EOF
+
 .print
 .print --> Get one file per checksum where foo IS NULL
-.mode column
-.headers on
 .width 4 8 20 4 20
 select a.id,a.checksum,a.foo,b.id,b.file_path 
 from checksums as a 
